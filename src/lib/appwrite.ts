05d3bf56481be6a2bc6e7667a0625f7dcc44b957
elgafr <elgaputrifr@gmail.com>
2025-01-01 22:11:18 +0700

    add members list

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -41,6 +41,9 @@ export async function createAdminClient() {
     return {
         get account() {
             return new Account(client)
+        },
+        get users() {
+            return new Users(client)
         }
     }
 }
