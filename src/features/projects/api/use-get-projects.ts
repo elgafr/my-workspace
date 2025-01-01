eab23270908340c8684b002c2f47955947be6cf6
elgafr <elgaputrifr@gmail.com>
2025-01-02 03:49:01 +0700

    add workspace projects

---
+++ src/features/projects/api/use-get-projects.ts
@@ -0,0 +1,29 @@
+import { client } from "@/lib/rcp";
+import { useQuery } from "@tanstack/react-query"
+
+
+interface UseGetProjectProps {
+    workspaceId: string;
+}
+
+
+export const useGetProjects = ({workspaceId}: UseGetProjectProps) => {
+    const query = useQuery({
+        queryKey: ["projects", workspaceId],
+        queryFn: async () => {
+            const response = await client.api.projects.$get({
+                query: {workspaceId},
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch projects")
+            }
+
+            const { data } = await response.json()
+
+            return data
+        }
+    })
+
+    return query
+}
