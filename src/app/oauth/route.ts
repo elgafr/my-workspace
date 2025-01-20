1d903225f3723c69e4cebb3c7caa5ec8a8c05e61
elgafr <elgaputrifr@gmail.com>
2025-01-21 05:29:51 +0700

    Update route.ts

--- src/app/oauth/route.ts
+++ src/app/oauth/route.ts
@@ -1,6 +1,5 @@
 import { AUTH_COOKIE } from "@/features/auth/constants";
 import { createAdminClient } from "@/lib/appwrite";
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
