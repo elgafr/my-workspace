b566c413ba2f07ffce75ff8c17affcc3eea1173a
elgafr <elgaputrifr@gmail.com>
2025-01-21 05:45:14 +0700

    Update route.ts

--- src/app/oauth/route.ts
+++ src/app/oauth/route.ts
@@ -1,29 +1,25 @@
 import { AUTH_COOKIE } from "@/features/auth/constants";
 import { createAdminClient } from "@/lib/appwrite";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const userId = request.nextUrl.searchParams.get("userId");
   const secret = request.nextUrl.searchParams.get("secret");
 
-  if (!userId || !secret) {
-    return new NextResponse("Missing fields", { status: 400 });
+  if(!userId || !secret){
+    return new NextResponse("Missing fields", { status: 400 })
   }
 
   const { account } = await createAdminClient();
   const session = await account.createSession(userId, secret);
 
-  // Create a new response with redirection
-  const response = NextResponse.redirect(`${request.nextUrl.origin}/`);
-
-  // Set the cookie on the response object
-  response.cookies.set(AUTH_COOKIE, session.secret, {
+  cookies().set(AUTH_COOKIE, session.secret, {
     path: "/",
-    domain: ".elga.my.id", // Allows subdomains
     httpOnly: true,
     sameSite: "lax",
     secure: true,
   });
 
-  return response;
+  return NextResponse.redirect(`${request.nextUrl.origin}/`);
 }
